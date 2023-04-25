5a00b6fca819af597c31d95bfbf40a5fe4160bc4
Andrey Seluykov <trezortop92@gmail.com>
2023-04-26 03:10:40 +0500

    update

---
+++ stl/queue.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <list>
+#include <queue>
+#include <string>
+
+int main() {
+  std::priority_queue<int> priorityQueue;
+
+  priorityQueue.push(1);
+  priorityQueue.push(2);
+  priorityQueue.push(3);
+  priorityQueue.push(4);
+
+  priorityQueue.emplace(5);
+
+  priorityQueue.pop();
+
+  std::cout << "priorityQueue.top() " << priorityQueue.top() << '\n';
+
+  while (!priorityQueue.empty())
+    priorityQueue.pop();
+
+  std::cout << "priorityQueue.size() " << priorityQueue.size() << '\n';
+
+  return 0;
+}
